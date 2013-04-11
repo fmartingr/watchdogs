c06180a0cd801f4fa4884a27c7296c8eb488fd4e
Felipe Martín <me@fmartingr.com>
2013-04-11 12:45:23 +0200

    Fixed binary files naming

--- utils/build.sh
+++ utils/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-coffee -c -p coffee/server.coffee > bin/watchdogs_server.js
-coffee -c -p coffee/client.coffee > bin/watchdogs_client.js
+
+# Server
+echo "#!/usr/bin/env node" > bin/watchdogs_server
+coffee -c -p coffee/server.coffee >> bin/watchdogs_server
+
+# Client
+echo "#!/usr/bin/env node" > bin/watchdogs_client
+coffee -c -p coffee/client.coffee >> bin/watchdogs_client
+
+# Viewer
 coffee -c -p coffee/viewer.coffee > lib/viewer.js
